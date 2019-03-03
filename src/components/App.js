cc5a3378dd5f74f719e1b1116e4008add1d968b4
Stephanie Smith <stephsmithcode@gmail.com>
2019-03-03 12:40:32 -0800

    starter files

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+function App(props) {
+  return (
+    <div>
+      Hello!
+    </div>
+  )
+}
+
+App.propTypes = {
+
+}
+
+export default App
