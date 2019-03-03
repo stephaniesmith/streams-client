b5c48ba24a971722350c8d3110679b910fbb4926
Stephanie Smith <stephsmithcode@gmail.com>
2019-03-03 13:48:19 -0800

    router setup

--- src/components/App.js
+++ src/components/App.js
@@ -2,12 +2,23 @@ import React from 'react';
 import { BrowserRouter, Route, Link } from 'react-router-dom';
 
 import PropTypes from 'prop-types';
+import StreamCreate from './streams/StreamCreate';
+import StreamEdit from './streams/StreamEdit';
+import StreamDelete from './streams/StreamDelete';
+import StreamList from './streams/StreamList';
+import StreamShow from './streams/StreamShow';
 
 const App = () => {
   return (
-    <div className="ui container">
-      Hello!
-    </div>
+    <BrowserRouter>
+      <>
+        <Route path="/" exact component={StreamList}/>
+        <Route path="/stream/new" component={StreamCreate}/>
+        <Route path="/stream/edit" component={StreamEdit}/>
+        <Route path="/stream/delete" component={StreamDelete}/>
+        <Route path="/stream/show" component={StreamShow}/>
+      </>
+    </BrowserRouter>
   );
 };
 
