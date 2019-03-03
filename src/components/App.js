6b0a70782359aec9e5126c991f284ed0f50bf7db
Stephanie Smith <stephsmithcode@gmail.com>
2019-03-03 12:43:20 -0800

    more setup

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,18 @@
-import React from 'react'
-import PropTypes from 'prop-types'
+import React from 'react';
+import { BrowserRouter, Route, Link } from 'react-router-dom';
 
-function App(props) {
+import PropTypes from 'prop-types';
+
+const App = () => {
   return (
     <div>
       Hello!
     </div>
-  )
-}
+  );
+};
 
 App.propTypes = {
 
-}
+};
 
-export default App
+export default App;
