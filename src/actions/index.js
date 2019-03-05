7ef8d31590639fd6f6b5703a9834a31b38693c5b
Stephanie Smith <stephsmithcode@gmail.com>
2019-03-04 19:17:37 -0800

    action refactor

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,11 +1,14 @@
+export const SIGN_IN = 'SIGN_IN';
+export const SIGN_OUT = 'SIGN_OUT';
+
 export const signIn = () => {
   return {
-    type: 'SIGN_IN'
+    type: SIGN_IN
   };
 };
 
 export const signOut = () => {
   return {
-    type: 'SIGN_OUT'
+    type: SIGN_OUT
   };
 };
--- src/reducers/auth.js
+++ src/reducers/auth.js
@@ -1,12 +1,14 @@
+import { SIGN_IN, SIGN_OUT } from '../actions';
+
 const initialState = {
   isSignedIn: null
 };
 
 export default (state = initialState, { type, payload }) => {
   switch(type) {
-    case 'SIGN_IN':
+    case SIGN_IN:
       return { ...state, isSignedIn: true };
-    case 'SIGN_OUT':
+    case SIGN_OUT:
       return { ...state, isSignedIn: false };
     default:
       return state;
