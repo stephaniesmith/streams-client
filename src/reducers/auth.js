2a2bc760d7fe9374b31b7cb568d98a0a6261243a
Stephanie Smith <stephsmithcode@gmail.com>
2019-03-04 19:08:21 -0800

    auth reducers

---
+++ src/reducers/auth.js
@@ -0,0 +1,14 @@
+const initialState = {
+  isSignedIn: null
+};
+
+export default (state = initialState, { type, payload }) => {
+  switch(type) {
+    case 'SIGN_IN':
+      return { ...state, isSignedIn: true };
+    case 'SIGN_OUT':
+      return { ...state, isSignedIn: false };
+    default:
+      return state;
+  }
+};
